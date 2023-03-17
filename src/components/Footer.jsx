f317e5eb314989cc51a3d2774e2dc91ee73ed70b
bealecs <109564941+bealecs@users.noreply.github.com>
2023-03-16 23:39:46 -0500

    added a temporary footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './css/Footer.css';
+
+export const Footer = () => {
+    
+    return(
+        <>
+        <div className="first-container">
+        <h1>This will be my footer</h1>
+        <div className="footer-container">
+            <div className="footer-buttons">*button here*</div>
+            <div className="footer-buttons">*button here*</div>
+            <div className="footer-buttons">*button here*</div>
+        </div>
+        </div>
+        </>
+    )
+}
